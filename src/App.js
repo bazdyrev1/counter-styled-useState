f028b68bfa7299bee41b124783a667aee511a9f9
Влад Влад <bazdyrev1@gmail.com>
2021-12-25 16:02:15 +0200

    count finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    display: flex;
+    width: 200px;
+    height: 200px;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    align-content: center;
+    margin: auto;
+    ${(props) =>
+      props.sizeS &&
+      css`
+        background: rgb(40, 110, 250);
+        width: 30px;
+        height: 30px;
+        margin: 0;
+        border-radius: 5px;
+      `}
+  `;
+
+  const Button = styled.button`
+    background: transparent;
+    border: 0px solid transparent;
+    color: rgb(40, 110, 250);
+    padding: 0.5em 1em;
+    :active {
+      color: rgb(151, 185, 252);
+    }
+    margin: 0px;
+  `;
+  const translateYP = keyframes`
+  from{
+    transform: translateY(30px);
+  }
+  to {
+    transform: translateY(0px)
+  }
+  `;
+  const translateYM = keyframes`
+  from{
+    transform: translateY(-30px);
+  }
+  to {
+    transform: translateY(0px)
+  }
+  `;
+  const Counter = styled.p`
+    color: white;
+    font-size: 20px;
+    animation: ${(props) => props.anim} 0.5s;
+  `;
+
+  const [count, setCount] = useState(0);
+  const [animations, setAnimations] = useState(translateYP);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const dencrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container>
+        <Button
+          onClick={() => {
+            dencrement();
+            setAnimations(translateYM);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </Button>
+
+        <Container sizeS>
+          <Counter anim={animations}>{count}</Counter>
+        </Container>
+
+        <Button
+          onClick={() => {
+            increment();
+            setAnimations(translateYP);
+          }}
+        >
+          +
+        </Button>
+      </Container>
     </div>
   );
 }
